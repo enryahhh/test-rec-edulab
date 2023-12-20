ca1f76557f233a613951ed1291f96fef5a0bd575
enryahhh <linggaj28@gmail.com>
2023-12-20 14:16:10 +0700

    fix : ganti nama route sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -20,7 +20,7 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
                 <li class="nav-item">
-                    <a href="/dashboard" class="nav-link">
+                    <a href="{{route('student.index')}}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Siswa</p>
                     </a>
